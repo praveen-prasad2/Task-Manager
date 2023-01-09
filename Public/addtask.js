ec3bb272b101b9cfa86bf847c127898bd6813d24
Praveen-Prasad <praveenprasad2510@gmail.com>
2023-01-09 09:06:34 +0530

    called api using axios

---
+++ Public/addtask.js
@@ -0,0 +1,20 @@
+
+
+
+    const form=document.querySelector("#form")
+    form.addEventListener("submit" ,(e)=>{
+        e.preventDefault()
+        let formdata=document.querySelector(".taskname")
+        console.log(formdata.value)
+    })
+
+
+
+
+
+
+    // let   response=await axios.post("http://localhost:5000/task")
+    // console.log(response.data)
+
+   
+
--- Public/index.js
+++ Public/index.js
@@ -1,3 +1,5 @@
+
+
 function getData() {
     console.log("Hi")
     fetch("http://localhost:5000/get-all-task")
@@ -9,7 +11,7 @@ function getData() {
                 const { alltask } = data;
                 // alltask.forEach(task => {console.log(task.taskname)});
                 let newArr = alltask.map((task) => {
-                    return ` <div class="cards">
+                    return ` 
             <div class="card">
             <div class="content">
               <div class="tools">
@@ -46,7 +48,7 @@ function getData() {
               </div>
               </div>
             </div>
-          </div>`}).join("")
+          `}).join("")
                 document.querySelector("#main").innerHTML = newArr
                 console.log(newArr);
             } else {
@@ -54,4 +56,63 @@ function getData() {
             }
         })
 }
-getData()
+// getData()
+
+async function  fetchData()  {
+ 
+ let   response=await axios.get("http://localhost:5000/get-all-task")
+ console.log(response.data)
+ {
+  if (response.data.success) {
+      // console.log(data.alltask)
+
+      const { alltask } = response.data;
+      // alltask.forEach(task => {console.log(task.taskname)});
+      let newArr = alltask.map((task) => {
+          return ` 
+  <div class="card">
+  <div class="content">
+    <div class="tools">
+      <div class="circles">
+        <div class="circle">
+          <span class="red box"></span>
+        </div>
+        <div class="circle">
+          <span class="yellow box"></span>
+        </div>
+        <div class="circle">
+          <span class="green box"></span>
+        </div>
+      </div>
+      <h5 class="taskname">${task.taskname}</h5>
+    </div>
+
+    <div class="cardcontent">
+      <p class="description">${task.completed ? `<del>${task.description}</del>` : task.description}</p>
+    </div>
+    <div class="options">
+      <button class="edit">
+        <span> <i class="fa-solid fa-pen-to-square"></i>
+        </span>
+      </button>
+      <button class="delete edit">
+        <span> <i class="fa-solid fa-trash"></i>
+        </span>
+      </button>
+      <label class="container ">
+        <input ${task.completed ? 'checked="checked" ' : ""} type="checkbox">
+        <div class="checkmark"></div>
+      </label>
+    </div>
+    </div>
+  </div>
+`}).join("")
+      document.querySelector("#main").innerHTML = newArr
+      console.log(newArr);
+  } else {
+      console.log(response.data.message)
+  }
+}
+}
+
+fetchData()
