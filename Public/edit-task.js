fa9507756894373a582e5fc641f7e73616df06b1
Praveen-Prasad <praveenprasad2510@gmail.com>
2023-01-12 09:15:53 +0530

    completed edit task

--- Public/edit-task.js
+++ Public/edit-task.js
@@ -6,18 +6,39 @@ const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id')
 console.log(id);
 
+let taskname=document.querySelector("#taskname")
+let description=document.querySelector('#description')
 
 async function getOneTask(){
     let onetask=await axios.get("http://localhost:5000/get-one-task/"+ id)
     console.log(onetask.data)
 
-    let taskname=document.querySelector("#taskname")
+   
     
  if(onetask.data.success){
     taskname.value=onetask.data.task.taskname
+    description.value=onetask.data.task.description
  }
 }
 
 
-
 getOneTask()
+
+
+const form= document.querySelector('#form')
+form.addEventListener("submit",async (e)=>{
+   e.preventDefault()
+   
+   let response=await axios.patch("http://localhost:5000/update-task/"+id,{taskname:taskname.value,description:description.value})
+   console.log(response.data)
+
+   if(response.data.success){
+
+      alert(response.data.message)
+      window.location.href="index.html"
+  }else{
+      alert(response.data.message)
+  }
+       
+        
+      })
